7f5bea3b882011665981417d6ad3dd29d3074c94
Harald Atteneder <harald@urbantrout.io>
2017-10-13 10:34:58 +0200

    Use Transliterator for generating HTML IDs

--- Classes/Utility/StringFormat.php
+++ Classes/Utility/StringFormat.php
@@ -1,6 +1,8 @@
 <?php
 namespace Onedrop\SectionMenu\Utility;
 
+use Behat\Transliterator\Transliterator;
+
 /**
  * Class StringFormat
  *
@@ -10,28 +12,13 @@ class StringFormat
 {
 
     /**
-     * Only keeps allowed characters in string for usage in
-     * HTML ID attribute.
-     *
-     * Note: You should already have removed html tags
+     * Transforms text into a slug.
      *
      * @param string $input
      * @return string
      */
     public static function htmlId($input)
     {
-        $input = str_replace(' ', '-', $input);
-        $input = str_replace('/', '-', $input);
-        $input = str_replace(',', '-', $input);
-        $input = str_replace('|', '-', $input);
-        $input = str_replace('.', '-', $input);
-        $input = str_replace('ä', 'ae', $input);
-        $input = str_replace('ü', 'ue', $input);
-        $input = str_replace('ö', 'oe', $input);
-        $input = preg_replace('/[^[:alnum:]]/', '', trim($input));
-        while (strpos($input, '--') !== false) {
-            $input = str_replace('--', '-', $input);
-        }
-        return $input;
+        return Transliterator::urlize($input);
     }
 }
